d173bc59a6f0468b260809288ae1882c721614ef
MahdiSelmani <selmani.mahdi.1995@gmail.com>
2022-03-28 19:43:56 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -42,11 +42,36 @@ function App() {
 
    const { data, isLoading, error } = useQuery<CartItemType[]>('products',
     getProducts);
-  console.log(data);
   const getTotalItems = (items: CartItemType[]) => 
   items.reduce((ack: number, item) => ack + item.amount, 0);
-  const handleAddToCart = (clickedItem: CartItemType) => null;
-  const handleRemoveFromCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      //  is the item already in cart ?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id 
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        ))
+      }
+      // first time the item is added 
+      return [...prev, { ...clickedItem, amount: 1}]
+
+    })
+  };
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, {...item , amount: item.amount - 1}]
+        } else {
+          return [...ack, item]
+        }
+      }, [] as CartItemType[])
+    ))
+  };
 
 
   if (isLoading) return <LinearProgress />;
